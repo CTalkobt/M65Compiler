588c242a8575746c06326f0b653f4fe2fb874c67
ctalkobt@ctalkobt.net <ctalkobt@ctalkobt.net>
2026-04-30 22:49:31 -0400

    Fix .var non-positional evaluation

--- src/main/AssemblerGenerator.cpp
+++ src/main/AssemblerGenerator.cpp
@@ -419,7 +419,13 @@ void AssemblerGenerator::generate(AssemblerParser* parser, M65Emitter& e) {
                 }
             } else if (stmt->type == AssemblerParser::Statement::DIRECTIVE) {
                 if (!isDeadCode || stmt->dir.name == "org") {
-                    if (stmt->dir.name == "var") { if (stmt->dir.varType == Directive::INC) parser->symbolTable[stmt->dir.varName].value++; else if (stmt->dir.varType == Directive::DEC) parser->symbolTable[stmt->dir.varName].value--; }
+                    if (stmt->dir.name == "var") {
+                        if (stmt->dir.varType == Directive::ASSIGN) {
+                            uint32_t val = parser->evaluateExpressionAt(stmt->dir.tokenIndex, stmt->scopePrefix);
+                            parser->symbolTable[stmt->dir.varName].value = val;
+                        } else if (stmt->dir.varType == Directive::INC) parser->symbolTable[stmt->dir.varName].value++;
+                        else if (stmt->dir.varType == Directive::DEC) parser->symbolTable[stmt->dir.varName].value--;
+                    }
                     else if (stmt->dir.name == "cleanup") { if (currentPass2Proc) currentPass2Proc->totalParamSize += parser->evaluateExpressionAt(stmt->dir.tokenIndex, stmt->scopePrefix); }
                     else if (stmt->dir.name == "byte") for (const auto& a : stmt->dir.arguments) e.emitByte((uint8_t)parseNumericLiteral(a));
                     else if (stmt->dir.name == "word") for (const auto& a : stmt->dir.arguments) e.emitWord((uint16_t)parseNumericLiteral(a));
